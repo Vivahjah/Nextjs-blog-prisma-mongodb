b4547e96747083683678966d7eba76751be7ad5e
Mengwa Ugo <mengwa44@yahoo.com>
2024-05-15 00:46:22 +0100

    x

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Menu from '@/components/menu/Menu'
 import styles from "./singlePage.module.css"
+import Image from 'next/image'
 const SinglePage   = () => {
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
           <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <div className={styles.user}>
+            <div className={styles.userImageContainer}>
+              <Image src="/p1.jpeg" alt='' fill className={styles.image}/>
+            </div>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src="/p1.jpeg" alt='' fill className={styles.image} />
         </div>
       </div>
       <div className={styles.content}>
